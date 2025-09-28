1a32053e0c78a649f7622391568a3c18e22c3709
thomation <thomation@126.com>
2025-09-28 14:35:54 +0800

    Wait child process exit

--- ptrace/first_ptrace.c
+++ ptrace/first_ptrace.c
@@ -6,26 +6,35 @@
 #include <stdio.h>
 #include <sys/reg.h>
 
-int main(void){
+int main(void)
+{
     pid_t child;
+    int status = 0;
     long orig_rax;
     child = fork();
-    if (child == 0){
-    	printf("Child process id: %d\n", getpid());
-	sleep(5);
+    if (child == 0)
+    {
+        printf("Child process id: %d\n", getpid());
+        sleep(1);
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-	sleep(5);
+        sleep(1);
         execl("/bin/ls", "ls", "-l", "-h", NULL);
-    } else {
-    	printf("Parent  process id: %d\n", getpid());
-    	printf("Parent wait id: %d\n", child);
-        wait(NULL);
-        printf("Child process called a system call\n");
+    }
+    else
+    {
+        printf("Parent  process id: %d\n", getpid());
+        printf("Parent wait id: %d\n", child);
+        wait(&status);
+        printf("Got signal %d\n", WSTOPSIG(status));
         orig_rax = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
         printf("Child process called a system call, id is %ld\n", orig_rax);
-	sleep(5);
+        sleep(1);
         ptrace(PTRACE_CONT, child, NULL, NULL);
         printf("ptrace cont %d\n", child);
+        // wait for child to stop again
+        sleep(1);
+        wait(&status);
+        printf("Got signal %d\n", WSTOPSIG(status));
     }
     printf("all done pid = %d\n", getpid());
     return 0;
